85ff536edf46de7d9ec84b0d8a47ce5bd2e1af72
ajanth-thangavelu-rillion <117822863+ajanth-thangavelu-rillion@users.noreply.github.com>
2025-01-17 10:07:38 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -76,7 +76,7 @@ const util_1 = __importDefault(__nccwpck_require__(669));
 const execAsync = util_1.default.promisify(child_process_1.exec);
 // Internal paths
 const certPath = `${process_1.env['TEMP']}\\certificate.pfx`;
-const signtool = 'C:/Program Files (x86)/Windows Kits/10/bin/10.0.22621.0/x86/signtool.exe';
+const signtool = 'C:/Program Files (x86)/Windows Kits/10/bin/10.0.22621.0/x64/signtool.exe';
 // Inputs
 const coreFolder = core.getInput('folder');
 const coreRecursive = core.getInput('recursive') === 'true';
